4cfc4d32f5b5e3ff571dab20330e801d95ad1156
M1k3Ha <mike.vlahavas2@gmail.com>
2026-01-14 17:31:11 +0200

    Add POI models

--- api/app/models.py
+++ api/app/models.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
+from typing import List, Optional
 
 class AboutMember(BaseModel):
     full_name: str
     am: str
+
 class LoginRequest(BaseModel):
     email: str
     password: str
@@ -14,3 +16,28 @@ class TokenResponse(BaseModel):
     access_token: str
     refresh_token: str
     token_type: str = "bearer"
+
+class CategoryOut(BaseModel):
+    id: str
+    title: str
+    count: int
+
+class PoiListItem(BaseModel):
+    id: str
+    title: str
+    lat: float
+    lon: float
+    image: Optional[str] = None
+    short: Optional[str] = None
+
+class PoiDetails(BaseModel):
+    id: str
+    category_id: str
+    category_title: str
+    title: str
+    short: Optional[str] = None
+    lat: float
+    lon: float
+    wikipedia_url: Optional[str] = None
+    images: List[str]
+    description: Optional[str] = None
